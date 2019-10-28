8ec9ac6904f6fa029c5d59222df3f47b1640bc07
Pierre <quentel.pierre@orange.fr>
2019-10-28 11:12:41 +0100

    Changement d'adresse du serveur (bind)

--- server.py
+++ server.py
@@ -1,2 +1,3 @@
 import http.server
-http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, port=8081)
+http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler, port=8081,
+    bind="192.168.1.37")
