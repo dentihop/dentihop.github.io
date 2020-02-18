3e7f8c2d2af71f359b98ff56c4e27563e5afd24b
PierreQuentel <quentel.pierre@orange.fr>
2020-02-18 14:22:04 +0100

    Changement dans l'ordre des illustrations (cf. document "Liste instruments appli avec métaphores.docx")

--- assets/illustrations/table.js
+++ assets/illustrations/table.js
@@ -2,19 +2,19 @@ table = {
     Premiere_consultation:{
         lib: "Première consultation",
         photographies:[
-            ['aspiration', 'aspiration.JPG', 'metaphores/bisou.png'],
             ['fauteuil', 'fauteuil.jpg', 'metaphores/manège métaphore fauteuil (premiere consultation).jpg'],
-            ['miroir', 'Miroir copie.JPG', 'metaphores/miroir métaphore.jpg'],
-            ['précelle', 'Précelles.JPG', 'metaphores/pince.jpg'],
             ['scialytique', 'scialytique.jpg', 'metaphores/soleil.jpg'],
+            ['miroir', 'Miroir copie.JPG', 'metaphores/miroir métaphore.jpg'],
             ['sonde', 'sonde 6.JPG', 'metaphores/crayon métaphore sonde (premiere consultation).jpg'],
+            ['précelle', 'Précelles.JPG', 'metaphores/pince.jpg'],
             ['soufflette', 'soufflette.jpg', 'metaphores/vent métaphore pour souflette.jpg'],
+            ['aspiration', 'aspiration.JPG', 'metaphores/bisou.png'],
             ['émetteur', 'émetteur radiologie.JPG', 'metaphores/appareil photo.jpg']
         ],
         pictogrammes:[
+            ['scialytique', 'scialytique.jpg', 'metaphores/soleil.jpg'],
             ['miroir', 'miroir-soinspréventions.png', 'metaphores/miroir.png'],
-            ['précelle', 'preěcelle-1ereconsult.png', 'metaphores/pince_a_linge.png'],
-            ['scialytique', 'scialytique.jpg', 'metaphores/soleil.jpg']
+            ['précelle', 'preěcelle-1ereconsult.png', 'metaphores/pince_a_linge.png']
         ]
     },
     Prevention:{
@@ -22,9 +22,9 @@ table = {
         dossier: "Prophylaxie",
         photographies:[
             ['brossette', 'Contre-angle et brossette.JPG', 'métaphores/brosse a dent.jpg'],
-            ['micro-brosse', 'Micro-brosses.png', 'métaphores/pinceau métaphore microbrosse.jpg'],
+            ['vernis', 'vernis fluoré.JPG', 'métaphores/banane métaphore vernis.jpg'],
             ['soufflette', 'soufflette.jpg', 'métaphores/vent métaphore pour souflette.jpg'],
-            ['vernis', 'vernis fluoré.JPG', 'métaphores/banane métaphore vernis.jpg']
+            ['micro-brosse', 'Micro-brosses.png', 'métaphores/pinceau métaphore microbrosse.jpg']
         ],
         pictogrammes:[
             ['contre-angle', 'contre-angle.jpg', 'métaphores/brosseadent.jpg'],
@@ -35,9 +35,9 @@ table = {
         lib: "Détartrage",
         dossier: "Detartrage",
         photographies:[
-            ['aspiration', 'aspiration.JPG', 'métaphores/bisou.png'],
+            ['détartreur', 'Detartreur.JPG', 'métaphores/pommeau de douche.jpg'],
             ['brossette', 'Contre-angle et brossette.JPG', 'métaphores/brosse a dent.jpg'],
-            ['détartreur', 'Detartreur.JPG', 'métaphores/pommeau de douche.jpg']
+            ['aspiration', 'aspiration.JPG', 'métaphores/bisou.png']
         ],
         pictogrammes:[
             ['détartreur', 'deětartreur.jpg', 'métaphores/brosseaÌdent.jpg'],
@@ -47,11 +47,11 @@ table = {
     Carie:{
         photographies:[
             ['anesthésie', 'anesthésie .JPG', 'métaphores/pompe a velo.jpg'],
-            ['composite', 'Composite.png', 'métaphores/peinture.jpg'],
             ['fraise sur contre-angle', 'fraise boule montée sur contre-angle.JPG', 'métaphores/tractopelle.jpg'],
             ['fraise sur turbine', 'fraise boule montée sur turbine.JPG', 'métaphores/pommeau de douche.jpg'],
-            ['lampe à photopolymériser', 'lampe à photopolymériser.JPG', 'métaphores/lampe bleue .png'],
-            ['spatule', 'spatule.JPG', 'métaphores/pinceau comme micro-brosse (dans soin de carie).jpg']
+            ['spatule', 'spatule.JPG', 'métaphores/pinceau comme micro-brosse (dans soin de carie).jpg'],
+            ['composite', 'Composite.png', 'métaphores/peinture.jpg'],
+            ['lampe à photopolymériser', 'lampe à photopolymériser.JPG', 'métaphores/lampe bleue .png']
         ],
         pictogrammes:[
             ['anesthésie', 'anesthesie.png', 'métaphores/pompe_a_velo.png'],
@@ -71,10 +71,11 @@ table = {
         dossier: "Avulsion",
         photographies:[
             ['anesthésie', 'anesthésie.JPG', 'metaphore/pompe a velo.jpg'],
-            ['compresse', 'compresse.JPG', 'metaphore/pansement.png'],
+            ['syndesmotome', 'syndesmotome.JPG', 
+                'metaphore/stylo plume métaphore syndesmotome.jpg'],
             ['curette', 'curette.JPG', 'metaphore/cuillère.jpg'],
             ['suture', 'suture.JPG', 'metaphore/couture.jpg'],
-            ['syndesmotome', 'syndesmotome.JPG', 'metaphore/stylo plume métaphore syndesmotome.jpg']
+            ['compresse', 'compresse.JPG', 'metaphore/pansement.png']
         ],
         pictogrammes:[
             ['compresse', 'compresses.jpg', 'metaphores/Pansement.jpg']
@@ -82,9 +83,12 @@ table = {
     },
     MEOPA:{
         photographies:[
-            ['bouteille', 'bouteille MEOPA.JPG', 'métaphores/rire (métaphore bouteille gaz).jpg'],
-            ['masque et ballon', 'masque et ballon MEOPA.png', 'métaphores/ourson masque et ballon (métaphore masque et ballon).jpg'],
-            ['masque', 'masque Meopa1.JPG', 'métaphores/masque tuba plongée (métaphore masque Meopa1).png']
+            ['bouteille', 'bouteille MEOPA.JPG',
+                'métaphores/rire (métaphore bouteille gaz).jpg'],
+            ['masque et ballon', 'masque et ballon MEOPA.png',
+                'métaphores/ourson masque et ballon (métaphore masque et ballon).jpg'],
+            ['masque', 'masque Meopa1.JPG',
+                'métaphores/masque tuba plongée (métaphore masque Meopa1).png']
         ],
         pictogrammes:[
         ]
